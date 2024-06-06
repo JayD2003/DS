99e2d026c2d9e5453a4ccba1b1122601d949733d
Jay <142610767+JayD2003@users.noreply.github.com>
2024-06-06 14:45:31 +0530

    Delete DS directory

--- DS/Data-Structures/Linked list/ll.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class ll {
-    public static void main(String args[]) {
-        LinkedList<String> list = new LinkedList<>();
-        list.addFirst("sai");
-        System.out.println(list);
-    }
-}
